553aad9805eaba5f95c2113436db7e2347315d55
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2023-05-17 21:17:45 -0700

    created image fetching function, displaying images in main section

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
+const { Configuration, OpenAIApi } = require("openai")
 
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+app.post('/images', async (req, res) => {
+  try {
+    const response = await openai.createImage({
+      prompt: req.body.message,
+      n: 3,
+      size: "1024x1024",
+    });
+    console.log(response.data.data)
+    res.send(response.data.data)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.listen(PORT, () => console.log('Your server is running on port ' + PORT))
--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-
+import { useState } from "react"
 
 const App = () => {
+  const [images, setImages] = useState(null)
+  const [value, setValue] = useState('')
   const surpriseOptions = [
     'a blue ostrich eating melon',
     'Darth Vader kissing an ewok',
     'the death star going bowling'
   ]
 
+  const getImages = async () => {
+    setImages(null)
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          message: value
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/images', options)
+      const data = await response.json()
+      console.log(data)
+      setImages(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const surpriseMe = () => {
+    setImages(null)
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
   return (
     <div className="app">
       <section className='search-section'>
         <p>Start with a detailed description 
-          <span className="surprise">Surprise me</span>
+          <span className="surprise" onClick={surpriseMe}>Surprise me</span>
         </p>
         <div className="input-container">
-          <input placeholder="A teddy bear skateboarding"/>
-          <button>Generate</button>
+          <input 
+            placeholder="A teddy bear skateboarding" 
+            onChange={e => setValue(e.target.value)}
+            value={value}
+          />
+          <button onClick={getImages}>Generate</button>
         </div>
       </section>
-      <section className="image-section"></section>
+      <section className="image-section">
+        {images?.map((image, index) => (
+          <img key={index} src={image.url} alt={`${value}`} />
+        ))}
+      </section>
     </div>
   );
 }
